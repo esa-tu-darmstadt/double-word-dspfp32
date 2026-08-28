cf7be54ad3da6e4b4ae536e7fb3340637df1d770
David Volz <volz.david@gmx.de>
2026-08-28 19:24:55 +0200

    added dsp58 && fma implementation sketch

--- dspfp32.sv
+++ dspfp32.sv
@@ -1,3 +1,135 @@
+module dsp58_ip#(
+    parameter A_REGS = 0,
+    parameter B_REGS = 0,
+    parameter M_REGS = 0
+) (
+    input logic aclk,
+    input logic [33:0] a,
+    input logic [23:0] b,
+    output logic [57:0] p
+);
+
+// DSP58: 58-bit Multi-Functional Arithmetic Block
+// Versal Prime Series
+// Xilinx HDL Language Template, version 2025.1
+DSP58 #(
+    // Feature Control Attributes: Data Path Selection
+    .AMULTSEL("A"),// Selects A input to multiplier (A, AD)
+    .A_INPUT("DIRECT"),// Selects A input source, "DIRECT" (A port) or "CASCADE" (ACIN port)
+    .B_INPUT("DIRECT"),// Selects B input source, "DIRECT" (B port) or "CASCADE" (BCIN port)
+    .BMULTSEL("B"),// Selects B input to multiplier (AD, B)
+    .DSP_MODE("INT24"),// Configures DSP to a particular mode of operation. Set to INT24 for legacy mode.
+    .PREADDINSEL("A"),// Selects input to pre-adder (A, B)
+    .RND(58'h000000000000000),// Rounding Constant
+    .USE_MULT("MULTIPLY"),// Select multiplier usage (DYNAMIC, MULTIPLY, NONE)
+    .USE_SIMD("ONE58"),// SIMD selection (FOUR12, ONE58, TWO24)
+    .USE_WIDEXOR("FALSE"),// Use the Wide XOR function (FALSE, TRUE)
+    .XORSIMD("XOR24_34_58_116"),// Mode of operation for the Wide XOR (XOR12_22, XOR24_34_58_116)
+    // Pattern Detector Attributes: Pattern Detection Configuration
+    .AUTORESET_PATDET("NO_RESET"),// NO_RESET, RESET_MATCH, RESET_NOT_MATCH
+    .AUTORESET_PRIORITY("RESET"),// Priority of AUTORESET vs. CEP (CEP, RESET).
+    .MASK(58'h0ffffffffffffff),// 58-bit mask value for pattern detect (1=ignore)
+    .PATTERN(58'h000000000000000),// 58-bit pattern match for pattern detect
+    .SEL_MASK("MASK"),// C, MASK, ROUNDING_MODE1, ROUNDING_MODE2
+    .SEL_PATTERN("PATTERN"),// Select pattern value (C, PATTERN)
+    .USE_PATTERN_DETECT("NO_PATDET"),// Enable pattern detect (NO_PATDET, PATDET)
+    // Programmable Inversion Attributes: Specifies built-in programmable inversion on specific pins
+    .IS_ALUMODE_INVERTED(4'b0000),// Optional inversion for ALUMODE
+    .IS_CARRYIN_INVERTED(1'b0),// Optional inversion for CARRYIN
+    .IS_CLK_INVERTED(1'b0),// Optional inversion for CLK
+    .IS_INMODE_INVERTED(5'b00000),// Optional inversion for INMODE
+    .IS_NEGATE_INVERTED(3'b000),// Optional inversion for NEGATE
+    .IS_OPMODE_INVERTED(9'b000000000),// Optional inversion for OPMODE
+    .IS_RSTALLCARRYIN_INVERTED(1'b0),// Optional inversion for RSTALLCARRYIN
+    .IS_RSTALUMODE_INVERTED(1'b0),// Optional inversion for RSTALUMODE
+    .IS_RSTA_INVERTED(1'b0),// Optional inversion for RSTA
+    .IS_RSTB_INVERTED(1'b0),// Optional inversion for RSTB
+    .IS_RSTCTRL_INVERTED(1'b0),// Optional inversion for STCONJUGATE_A
+    .IS_RSTC_INVERTED(1'b0),// Optional inversion for RSTC
+    .IS_RSTD_INVERTED(1'b0),// Optional inversion for RSTD
+    .IS_RSTINMODE_INVERTED(1'b0),// Optional inversion for RSTINMODE
+    .IS_RSTM_INVERTED(1'b0),// Optional inversion for RSTM
+    .IS_RSTP_INVERTED(1'b0),// Optional inversion for RSTP
+    // Register Control Attributes: Pipeline Register Configuration
+    .ACASCREG(1),// Number of pipeline stages between A/ACIN and ACOUT (0-2)
+    .ADREG(1),// Pipeline stages for pre-adder (0-1)
+    .ALUMODEREG(1),// Pipeline stages for ALUMODE (0-1)
+    .AREG(A_REGS),// Pipeline stages for A (0-2)
+    .BCASCREG(1),// Number of pipeline stages between B/BCIN and BCOUT (0-2)
+    .BREG(B_REGS),// Pipeline stages for B (0-2)
+    .CARRYINREG(1),// Pipeline stages for CARRYIN (0-1)
+    .CARRYINSELREG(1),// Pipeline stages for CARRYINSEL (0-1)
+    .CREG(1),// Pipeline stages for C (0-1)
+    .DREG(1),// Pipeline stages for D (0-1)
+    .INMODEREG(1),// Pipeline stages for INMODE (0-1)
+    .MREG(M_REGS),// Multiplier pipeline stages (0-1)
+    .OPMODEREG(1),// Pipeline stages for OPMODE (0-1)
+    .PREG(1),// Number of pipeline stages for P (0-1)
+    .RESET_MODE("SYNC")// Selection of synchronous or asynchronous reset. (ASYNC, SYNC).
+) DSP58_inst (
+    // Cascade outputs: Cascade Ports
+    .ACOUT(),// 34-bit output: A port cascade
+    .BCOUT(),// 24-bit output: B cascade
+    .CARRYCASCOUT(),// 1-bit output: Cascade carry
+    .MULTSIGNOUT(),// 1-bit output: Multiplier sign cascade
+    .PCOUT(),// 58-bit output: Cascade output
+    // Control outputs: Control Inputs/Status Bits
+    .OVERFLOW(),// 1-bit output: Overflow in add/acc
+    .PATTERNBDETECT(),// 1-bit output: Pattern bar detect
+    .PATTERNDETECT(),// 1-bit output: Pattern detect
+    .UNDERFLOW(),// 1-bit output: Underflow in add/acc
+    // Data outputs: Data Ports
+    .CARRYOUT(),// 4-bit output: Carry
+    .P(p),// 58-bit output: Primary data
+    .XOROUT(),// 8-bit output: XOR data
+    // Cascade inputs: Cascade Ports
+    .ACIN(0),// 34-bit input: A cascade data
+    .BCIN(0),// 24-bit input: B cascade
+    .CARRYCASCIN(0),// 1-bit input: Cascade carry
+    .MULTSIGNIN(0),// 1-bit input: Multiplier sign cascade
+    .PCIN(0),// 58-bit input: P cascade
+    // Control inputs: Control Inputs/Status Bits
+    .ALUMODE(0),// 4-bit input: ALU control
+    .CARRYINSEL(0),// 3-bit input: Carry select
+    .CLK(aclk),// 1-bit input: Clock
+    .INMODE(0),// 5-bit input: INMODE control
+    .NEGATE(0),// 3-bit input: Negates the input of the multiplier
+    .OPMODE(0),// 9-bit input: Operation mode
+    // Data inputs: Data Ports
+    .A(a),// 34-bit input: A data
+    .B(b),// 24-bit input: B data
+    .C(0),// 58-bit input: C data
+    .CARRYIN(0),// 1-bit input: Carry-in
+    .D(0),// 27-bit input: D data
+    // Reset/Clock Enable inputs: Reset/Clock Enable Inputs
+    .ASYNC_RST(0),// 1-bit input: Asynchronous reset for all registers.
+    .CEA1(1'b1),// 1-bit input: Clock enable for 1st stage AREG
+    .CEA2(1'b1),// 1-bit input: Clock enable for 2nd stage AREG
+    .CEAD(1'b1),// 1-bit input: Clock enable for ADREG
+    .CEALUMODE(1'b1),// 1-bit input: Clock enable for ALUMODE
+    .CEB1(1'b1),// 1-bit input: Clock enable for 1st stage BREG
+    .CEB2(1'b1),// 1-bit input: Clock enable for 2nd stage BREG
+    .CEC(1'b1),// 1-bit input: Clock enable for CREG
+    .CECARRYIN(1'b1),// 1-bit input: Clock enable for CARRYINREG
+    .CECTRL(1'b1),// 1-bit input: Clock enable for OPMODEREG and CARRYINSELREG
+    .CED(1'b1),// 1-bit input: Clock enable for DREG
+    .CEINMODE(1'b1),// 1-bit input: Clock enable for INMODEREG
+    .CEM(1'b1),// 1-bit input: Clock enable for MREG
+    .CEP(1'b1),// 1-bit input: Clock enable for PREG
+    .RSTA(1'b0),// 1-bit input: Reset for AREG
+    .RSTALLCARRYIN(1'b0),// 1-bit input: Reset for CARRYINREG
+    .RSTALUMODE(1'b0),// 1-bit input: Reset for ALUMODEREG
+    .RSTB(1'b0),// 1-bit input: Reset for BREG
+    .RSTC(1'b0),// 1-bit input: Reset for CREG
+    .RSTCTRL(1'b0),// 1-bit input: Reset for OPMODEREG and CARRYINSELREG
+    .RSTD(1'b0),// 1-bit input: Reset for DREG and ADREG
+    .RSTINMODE(1'b0),// 1-bit input: Reset for INMODE register
+    .RSTM(1'b0),// 1-bit input: Reset for MREG
+    .RSTP(1'b0)// 1-bit input: Reset for PREG
+);
+
+endmodule
+
 module dspfp32_ip#(
     parameter A_REGS = 0,
     parameter AC_REGS = 0,
